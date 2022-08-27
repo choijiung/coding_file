05f3c456d581714cdea7a595fd0d740a902c2275
logit <7jiung@gmail.com>
2022-08-27 20:01:57 +0900

    오타 변경

--- python/streamlit/streamlit.py
+++ python/streamlit/streamlit.py
@@ -3,7 +3,7 @@ veiw = [100, 150, 30]
 st.write("# Yotube view")
 st.write("## raw")
 veiw
-st.write("## bat chart")
+st.write("## bar chart")
 st.bar_chart(veiw)
 import pandas as pd
 sview = pd.Series(veiw)
