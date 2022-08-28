2d43a5c6a8a5e8e6a7489ba2d97905cdde5f1ad7
logit <7jiung@gmail.com>
2022-08-28 21:32:25 +0900

    changes

--- python/streamlit/streamlit.py
+++ python/streamlit/streamlit.py
@@ -1,10 +1,10 @@
 import streamlit as st
+import pandas as pd
 veiw = [100, 150, 30]
+sheetview = pd.Series(veiw)
 st.write("# Youtube view")
 st.write("## raw")
 veiw
 st.write("## bar chart")
 st.bar_chart(veiw)
-import pandas as pd
-sview = pd.Series(veiw)
 sview
