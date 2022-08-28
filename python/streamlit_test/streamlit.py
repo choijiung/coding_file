0c262cf48b72601a0efa1f9a76ea91513b707a98
logit <7jiung@gmail.com>
2022-08-28 22:03:06 +0900

    코로나 확진자 수 사이트로 바꿈

--- python/streamlit_test/streamlit.py
+++ python/streamlit_test/streamlit.py
@@ -1,11 +1,14 @@
 import streamlit as st
 import pandas as pd
-veiw = [100, 150, 30]
+veiw = [17494, 11495, 13430, 18938, 17902, 16109, 41401, 53121, 59899, 53450, 822565, 183680, 215009, 22889479, 9962498, 4143373, 866392, 256101, 1407983]
 sheetview = pd.Series(veiw)
-st.write("# Youtube view")
-st.write("## raw")
+st.write("# This is a test app")
+st.write("# Covid - 19(monthly)")
+st.write("## raw(from 2021 -January)")
 veiw
 st.write("## bar chart")
 st.bar_chart(veiw)
+st.write("## line chart")
+st.line_chart(veiw)
 st.write("## pandas sheet")
 sheetview
